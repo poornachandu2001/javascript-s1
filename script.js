9477b9f7fcb18c29f639130ae4be0dcaef80ecf1
poornachandu2001 <99313312+poornachandu2001@users.noreply.github.com>
2022-03-13 18:44:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+console.log("hello world")
+
+//data types
+//string - alphabets
+//int - whole numbers
+//float - decimal numbers
+//boolean - true or false
+
+console.log("My nam is chandu")
+console.log("My age is 21")
+console.log("My height is 6.2")
+console.log("The statement Im studying in klu is true")
+//variables
+//variables can be modified any number of times
+var n = "chandu"   //string
+var a = 21     //integer
+var h = 6.2   //float
+var an = true     //boolean
+
+console.log(a)
+console.log("n")
+console.log("My nam is " + n)
+console.log("Im " +a+" years old")
+console.log("My height is " +h)
+console.log("The statement Im studying in klu is " +an)
+
+//let value, once declared cannot be modified
+let t = 35
+console.log(t)
+//let t = 40
+//console.log(t)
+
+let d
+d = "chandu"
+console.log(d)
+
+//const should be declared in a single line 
+
+var m = 21
+var y = 45
+const o = m+y
+console.log(o)
+
+//Area of a circle
+var r = 21
+let pi = 3.14
+const ar = pi*r*r
+console.log(ar)
+
+//Circumference of a circle
+var ra = 21
+let p = 3.14
+const cir = 2*p*ra
+console.log(cir)
+
+//radius of a circle
+var radius 
+radius = 24
+console.log(radius)
+
